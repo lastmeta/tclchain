b33aa0baf33f99857b236111cac522192bb46a8d
Jordan Miller <jordan.kay@gmail.com>
2016-08-23 20:18:03 -0600

    added inspect, made namespace

--- chain.tcl
+++ chain.tcl
@@ -16,29 +16,3 @@ namespace eval ::chain {
     return {*}$args
   }
 }
-
-  #proc needstwo {a b} {
-  #  return "worked! $a $b"
-  #}
-  #proc returnlist {args} {
-  #  return [list [lindex $args 0] "abc"]
-  #}
-
-  #proc return1 {} {
-  #  return Buddy
-  #}
-  #puts [chain "hello world"     \
-  [list string toupper {} ] \
-  [list inspect {}]          \
-  [list string tolower {}] \
-  [list inspect {}]         \
-  [list returnlist {}] \
-  [list inspect {}]         \
-  [list needstwo {} [return1]] \
-  [list inspect {}]         ]
-
-
-
-# next steps:
-# we'd really like to make it able to pass multiple parameters
-# maybe by using lassign or {*} with special caharacters like {***}, etc.
